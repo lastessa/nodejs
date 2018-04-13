f6ed758a5352cc2693aee816a2e15d8f1fd92779
Denis Derevyanchuk <denis@derevyanchuk.com>
2018-04-13 13:05:44 +0300

    1222

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   //res.send('k8s pod is OK\n');
-  res.send(err, server.address());
+  res.send(server.address());
 });
 
 app.listen(PORT, HOST);
